eff212436e455e925d97d58974076368fec3035f
Chihyu <chihyu10@gmail.com>
2020-03-09 09:53:27 +0100

    Made the base controllers clean version

---
+++ src/Controller/AgentDashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AgentDashboardController extends AbstractController
+{
+    /**
+     * @Route("/agent/dashboard", name="agent_dashboard")
+     */
+    public function index()
+    {
+        return $this->render('agent_dashboard/index.html.twig', [
+            'controller_name' => 'AgentDashboardController',
+        ]);
+    }
+}
