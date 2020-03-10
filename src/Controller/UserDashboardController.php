e392dfb73729a613c63ed595219301843da720a0
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-03-10 15:11:53 +0100

    working query biaatttchchhhc

--- src/Controller/UserDashboardController.php
+++ src/Controller/UserDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Ticket;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,25 @@ class UserDashboardController extends AbstractController
         return $this->render('user_dashboard/index.html.twig', [
             'controller_name' => 'UserDashboardController',
         ]);
+    }
+
+    /**
+     * @Route("/ticket/{id}", name="product_show")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Ticket::class)
+            ->find($id);
+
+        if (!$product) {
+            return $this->render('manager/index.html.twig', ['product' => 'This is not here']);
+
+        }
+
+
+        return $this->render('manager/index.html.twig', ['product' => $product]);
     }
 }
