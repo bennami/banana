de7b82055d5f858146db50223a5ab1e447c25df7
Thibence <salencethibaut95@gmail.com>
2020-03-11 16:05:29 +0100

    so, finally pushing some stuff, heck yezz

--- src/Controller/AgentDashboardController.php
+++ src/Controller/AgentDashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Ticket;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +17,41 @@ class AgentDashboardController extends AbstractController
         return $this->render('agent_dashboard/index.html.twig', [
             'controller_name' => 'AgentDashboardController',
         ]);
+    }
+
+    public function getOpenTicket($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findBy(['status' => 'Open']);
+
+        if (!$product) {
+            return $this->render('agent_dashboard/index.html.twig', ['product' => 'This is not here']);
+        }
+        $tickets = [];
+        foreach ($product->getTicketCreated() AS $ticket){
+            array_push($tickets, $ticket->getSubject());
+        }
+
+
+        return $this->render('agent_dashboard/index.html.twig', ['product' => $tickets]);
+    }
+
+    public function show($id)
+    {
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find($id);
+
+        if (!$user) {
+            return $this->render('agent/index.html.twig', ['product' => 'This is not here']);
+        }
+        $tickets = [];
+        foreach ($user->getTicketCreated() AS $ticket){
+            array_push($tickets, $ticket->getSubject());
+    }
+
+
+        return $this->render('agent_dashboard/index.html.twig', ['product' => $tickets]);
     }
 }
